45105fbf23359ffc43a5f6382fd720db5a86a2bd
TimKoster <timkoster14@gmail.com>
2026-09-04 13:42:17 +0200

    Replaces data_collector.py with the tcmu python module

--- smartqueue/queue_controller.py
+++ smartqueue/queue_controller.py
@@ -6,8 +6,7 @@ import subprocess
 import random
 import os
 import tcmu
-
-from data_collector import scrape_results
+from scm import plams
 
 relative_queued_path = "_queued"
 relative_finished_path = "_finished"
@@ -271,7 +270,7 @@ def gather_results(write_file, job:JobInfo):
     print("Gathering results for job", job.job_id, job.display_name)
 
     calculation_directory = Path(job.input_path).parent
-    results, seperate_results = scrape_results(calculation_directory, job.job_id)
+    results, seperate_results = get_results(calculation_directory)
 
     # Write neatly into the finished job file
     for key in results:
@@ -283,7 +282,7 @@ def gather_results(write_file, job:JobInfo):
             new_file.write(seperate_results[extension])
             print("New file added:", new_file.name)
 
-    # Write to excel in case we're not anarchists
+    # Write to excel in case we're not derailed anarchists
     excel = pd.read_excel(relative_excel_path)
 
     presentable_dict = {"status": results["status"], "job_id":str(job.job_id), "name":job.display_name, "path":job.input_path}
@@ -297,16 +296,48 @@ def gather_results(write_file, job:JobInfo):
         excel.to_excel(writer, sheet_name = 'Jobs', index = False)
         print("Updated", relative_excel_path)
 
-# def get_results(calculation_directory, job.job_id):
-#     results = dict()
-#     seperate_results = dict()
 
-#     result_object = tcmu.read(calculation_directory)
+# Extreme shame function. I forgot to wait for this to get merged before asking for a new tcmu release. Use tcmu.xyz_format whenever https://github.com/TheoChem-VU/TCMU/pull/534 is merged and in tcmu
+# and then get rid of this
+def xyz_format(mol: plams.Molecule, include_n_atoms: bool = True, include_lattices: bool = True) -> str:
+    """Returns a string representation of a molecule in the xyz format, e.g.:
 
-#     results["engine"] = result_object.engine
-#     results["status"] = tcmu.quick_status(calculation_directory)
-#     results["energy_out"] = result_object.properties.energy.bond
+    C      0.00000000      0.00000000      0.00000000
+    H      1.00000000      0.00000000      0.00000000
+    H      0.00000000      1.00000000      0.00000000
+    H      0.00000000      0.00000000      1.00000000
+    VEC1   1.00000000     -1.00000000      0.00000000
+    ...
+    """
 
+    xyz_string = ""
+
+    if include_n_atoms:
+        xyz_string = f"{len(mol.atoms)}\n"
+
+    xyz_string += "\n".join([f"{at.symbol:6s}{at.x:16.8f}{at.y:16.8f}{at.z:16.8f}" for at in mol.atoms])
+
+    if include_lattices and len(mol.lattice):
+        xyz_string += "\n" + "\n".join([f"VEC{str(i + 1):3s}{mol.lattice[i][0]:16.8f}{mol.lattice[i][1]:16.8f}{mol.lattice[i][2]:16.8f}" for i in range(0, len(mol.lattice))])
+
+    return xyz_string
+
+def get_results(calculation_directory:Path):
+    results = dict()
+    seperate_results = dict()
+
+    result_object = tcmu.read(calculation_directory)
+
+    results["engine"] = result_object.engine
+    results["task"] = result_object.input.Task
+    results["status"] = result_object.status.name.lower()
+    results["energy_out"] = result_object.properties.energy.bond
+
+    if result_object.molecule.output:
+        molecule:plams.Molecule = result_object.molecule.output
+        seperate_results["xyz"] = xyz_format(molecule)
+        
+    return results, seperate_results
 
 # Check what jobs are finished, and finish them if they are, then check the whole queue
 # For when you need to manually prompt a full update for one reason or another
